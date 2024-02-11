a585dd99b68f62e8757ea64b2c3eca3d24a7f8a9
Jean Jacques <35910266+FPDPanda@users.noreply.github.com>
2024-02-11 16:35:03 -0300

    Creating Episode component

---
+++ src/components/Episode.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+// CSS
+import "../styles/Episode.css";
+
+function Episode({ episodeNumber, episodeLocations, episodeCharacters }) {
+  let [openEpisode, setOpenEpisode] = useState(false);
+
+  function switchOpenEpisode() {
+    setOpenEpisode(!openEpisode);
+  }
+
+  return (
+    <button
+      onClick={switchOpenEpisode}
+      className={`App-main-screen-episode App-main-screen-episode--${
+        openEpisode ? "open" : "closed"
+      }`}
+    >
+      <div className="App-main-screen-episodeNumber">
+        Episode {episodeNumber}
+      </div>
+
+      <h2 className="App-main-screen-episodeTitles">Locations</h2>
+
+      <ul>
+        {episodeLocations.map((location) => (
+          <li className="App-main-screen-episode-location">{location.name}</li>
+        ))}
+      </ul>
+
+      <h2 className="App-main-screen-episodeTitles">Characters</h2>
+      <ul>
+        {episodeCharacters.map((character) => (
+          <li>
+            <img src={character.image} alt={character.name}></img>
+            <p>{character.name}</p>
+          </li>
+        ))}
+      </ul>
+    </button>
+  );
+}
+
+export default Episode;
